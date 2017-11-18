d35dd48bbb8ebbaa41790b385ed4036e547776bd
sivasatvik <sivasatvik@gmail.com>
2017-11-18 11:43:09 +0530

    Date-time predict fucntion

--- Satvik/run.py
+++ Satvik/run.py
@@ -1,5 +1,11 @@
 import datetime
-dt = '21-03-2012'
-day, month, year = (int(x) for x in dt.split('-'))
-ans = datetime.date(year, month, day)
-print ans.strftime("%A")
+
+## This function takes in input the date in the format(yyyy-mm-dd) and returns the day
+def tell_day(dt):
+    year, month, day = (int(x) for x in dt.split('-'))
+    ans = datetime.date(year, month, day)
+    print ans.strftime("%A")
+
+
+
+tell_day("2015-11-01")  # Sunday
